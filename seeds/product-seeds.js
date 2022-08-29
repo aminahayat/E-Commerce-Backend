b04db659e6c5aaa603b73019b4bee6ebb0bb4f7d
Amina Hayat <hayat.amina@hotmail.com>
2022-08-30 00:25:58 +0100

    complete i think

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -12,10 +12,27 @@ const productData = [
     price: 90.0,
     stock: 25,
     category_id: 5,
-
+  },
     {
         product_name: 'Branded Baseball Hat',
         price: 22.99,
         stock: 12,
         category_id: 4,
       },
+      {
+        product_name: 'Top 40 Music Compilation Vinyl Record',
+        price: 12.99,
+        stock: 50,
+        category_id: 3,
+    },
+    {
+      product_name: 'Cargo Shorts',
+      price: 29.99,
+      stock: 22,
+      category_id: 2,
+    },
+  ];
+  
+  const seedProducts = () => Product.bulkCreate(productData);
+  
+  module.exports = seedProducts;
