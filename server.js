a3e066b9645e3c191a1aa83ad636833fc99875dc
Amina Hayat <hayat.amina@hotmail.com>
2022-08-30 01:18:19 +0100

    chnages

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 
 const sequelize = require("./config/connection")
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,8 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync().then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-})});
+})})
